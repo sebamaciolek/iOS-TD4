e85de7262402c29afaecd5b5de4e702ab4474b3a
MACIOLEK Sebastian <ms613363@s202p06m.iutnice.unice.fr>
2017-01-30 17:00:10 +0100

    Ajout du projet jusqu'au #2

---
+++ Pods/Target Support Files/Pods-DAM-TD4/Pods-DAM-TD4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DAM_TD4 : NSObject
+@end
+@implementation PodsDummy_Pods_DAM_TD4
+@end
---
+++ Pods/Target Support Files/Pods-DAM-TD4/Pods-DAM-TD4-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_DAM_TD4VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DAM_TD4VersionString[];
+
